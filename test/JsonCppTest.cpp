c8b5a1a3bd60dc0c090d46259eb02bc3e114df20
zdc <3491957218@qq.com>
2024-07-26 14:18:40 +0800

    --测试json数据为空的情况

--- test/JsonCppTest.cpp
+++ test/JsonCppTest.cpp
@@ -2,9 +2,16 @@
 
 #include "../JsonCpp/json.hpp"
 using json=nlohmann::json;
+using namespace std;
+
+#define print(a) cout<<(a)<<endl
 
 int main()
 {
     json t;
+    t=R"({"name":null,"ttt":123})"_json;
+    print(t);
+    print(t.at("name"));
+    print(t.at("name").is_null());
     return 0;
 }
